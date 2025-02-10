b4594260eafb1fcb28abf8916bdcac7841ec08cd
Jamie Falconer <mrfluffyoven@gmail.com>
2025-02-10 08:36:28 +0000

    Cleanup common mk

--- twrp_mt6789-common.mk
+++ twrp_mt6789-common.mk
@@ -1,10 +1,3 @@
-#
-# Copyright (C) 2024 The Android Open Source Project
-# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -12,7 +5,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit some common twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
-# Inherit from a05s device
+# Inherit from mt6789-common
 $(call inherit-product, device/samsung/mt6789-common/device.mk)
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/mt6789-common/recovery/root,recovery/root)
